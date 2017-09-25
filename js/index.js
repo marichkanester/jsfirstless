1b93eba44f25cd66f347291cb944d3a1e0fd51e3
Marichka <svitlananester@ukr.net>
2017-09-25 13:25:44 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,33 +1,31 @@
-
-
-
-function print(val){
-    document.write(val+'<br>');
+function print(val) {
+    document.write(val + '<br>');
 }
 
-    
-    const a=Number(prompt('Enter a'));
-    const b=Number(prompt('Enter b'));
-    const c=Number(prompt('Enter c'));
-    
-    function discr(a, b, c){
-        
-        return (b*b)-(4*a*c);
+
+const a = Number(prompt('Enter a'));
+const b = Number(prompt('Enter b'));
+const c = Number(prompt('Enter c'));
+
+function discr(a, b, c) {
+
+    return (b * b) - (4 * a * c);
+}
+
+function solve(a, b, c) {
+    const d = discr(a, b, c);
+    if (d > 0) {
+        const x1 = (-b - Math.sqrt(d)) /( 2 * a);
+        const x2 = (-b + Math.sqrt(d)) / (2 * a);
+        return 'x1=' + x1 + '<br>' + 'x2=' + x2;
     }
-     function solve(a, b, c){
-         const d=discr(a, b, c);
-        if(d>0){
-            const x1=(-b-Math.sqrt( d ))/2*a;
-            const x2=(-b+Math.sqrt( d ))/2*a;
-            return 'x1='+x1+'<br>'+'x2='+x2;
-        }
-        else if(d===0){
-            const x=(-b)/2*a;
-             return 'x='+x;
-        }
-        else{
-            return 'No roots';
-        }
+    else if (d === 0) {
+        const x = (-b) / 2 * a;
+        return 'x=' + x;
     }
-    const result=solve(a, b, c);
-    print(result);
+    else {
+        return 'No roots';
+    }
+}
+const result = solve(a, b, c);
+print(result);
